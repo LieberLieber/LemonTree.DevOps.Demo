56274ea6b8f5816cc48e0a818a8c07e85cb9d227
Daniel Siegl <41949368+danielsiegl@users.noreply.github.com>
2026-01-28 09:21:23 +0100

    #52 Move PowerShell scripts to scripts/PCSDeploymentScripts for better organization (#53)

--- scripts/PCSDeploymentScripts/CountDiagramImageMaps.ps1
+++ scripts/PCSDeploymentScripts/CountDiagramImageMaps.ps1
@@ -1,3 +1,8 @@
+# Copyright (c) LieberLieber Software GmbH
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Calls QueryModel.ps1 with the provided SELECT statement
 
 $scriptPath = Join-Path $PSScriptRoot 'QueryModelSqliteExe.ps1'
--- scripts/PCSDeploymentScripts/DeleteDiagramImageMaps.ps1
+++ scripts/PCSDeploymentScripts/DeleteDiagramImageMaps.ps1
@@ -1,3 +1,8 @@
+# Copyright (c) LieberLieber Software GmbH
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Deletes Diagram Images from the model database
 
 $scriptPath = Join-Path $PSScriptRoot 'QueryModelSqliteExe.ps1'
